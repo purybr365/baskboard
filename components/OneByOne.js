32f6b99f56ec397997506b84d28ed906a9f775e8
Pury <puryp365@gmail.com>
2022-05-03 22:18:16 -0300

    Tooltips, Footer and active users

--- components/OneByOne.js
+++ components/OneByOne.js
@@ -1,16 +1,22 @@
+import React from 'react'
+import Tooltip from '@mui/material/Tooltip'
+
 function OneByOne({ data }) {
   
   const number = (data[2] === "$_thousands") ?
-    <span className="text-white font-light text-3xl">$<span className="font-bold">{ (data[1]/1000).toFixed(1) }k</span></span>
+    <span className="text-white font-light text-amber-500 text-3xl">$<span className="font-bold text-white">{ (data[1]/1000).toFixed(1) }k</span></span>
     :
     <span className="text-white font-bold text-3xl">{ data[1] }</span>
     
   
   return (
-    <div className="p-5 m-2 rounded-lg bg-sky-800 text-center">
-      <span className="text-sky-400">{ data[0] }</span><br />
-      { number }
-    </div>
+    <Tooltip title={data[3]} placement="top" arrow>
+      <div className="p-5 m-2 rounded-lg bg-sky-800 text-center">
+        
+        <span className="text-sky-400">{ data[0] }</span><br />
+        { number }
+      </div>
+    </Tooltip>
     )
   }
 
--- components/TwobyTwoTable.js
+++ components/TwobyTwoTable.js
@@ -1,12 +1,54 @@
+import React from 'react'
+
 function TwobyTwoTable({ data }) {
+
+  const tBody = "";
+
+  // console.log({
+  //  data1: data[1],
+  //  data2: data[2],
+  //  data3: data[3].header1
+  //  });
+
+
+  //for (const indexPort in data[1]) {
+  //  console.log(data[1][indexPort]);
+  //}
+  {/*  const tBody = data[1].forEach(function(portfolio) {
+      <tr>
+        <td className="text-white font-light text-sm">{portfolio.nftId}</td>
+        <td className="text-white font-light text-sm">{portfolio.createdOn}</td>
+        <td className="text-white font-light text-sm">{portfolio.cached.value}</td>
+      </tr>
+    }); */}
+
+  return (
+    {/*<div className="col-span-2 p-5 m-2 rounded-lg bg-sky-800 text-center">
+      <span className="text-sky-400">Latest portfolios</span><br />
+
     
-    return (
-      <div className="col-span-2 p-5 m-2 rounded-lg bg-sky-800 text-center">
-        <span className="text-sky-400">{ data[0] }</span><br />
-        <span className="text-white font-bold text-3xl">TBD</span>
-      </div>
-      
-      )
-    }
+
+      <table className="bg-red-500 text-center w-full">
+        <thead className="">
+            <td><span className="text-white font-bold text-sm">{data[3].header1}</span></td>
+            <td><span className="text-white font-bold text-sm">{data[3].header2}</span></td>
+            <td><span className="text-white font-bold text-sm">{data[3].header3}</span></td>
+        </thead>
+        <tbody>
+          {console.log(data[1])}
+          {data[1].map((portfolio,key) => {
+            console.log({"teste":portfolio[1], "item":i});
+            return (
+              <tr>
+                <td className="text-white font-light text-sm">{portfolio[key].nftId}</td>
+                <td className="text-white font-light text-sm">{portfolio[key].createdOn}</td>
+                <td className="text-white font-light text-sm">{portfolio[key].cached.value}</td>
+              </tr>
+            )}
+          )}
+        </tbody>              
+      </table>
+            </div>*/}
+  )}
   
   export default TwobyTwoTable
