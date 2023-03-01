1d9a8a9d795977337e9fbae740358fb8ca6865ca
Pury <puryp365@gmail.com>
2023-03-01 15:16:31 -0300

    Change data source to dev

--- framework/get-data.js
+++ framework/get-data.js
@@ -4,7 +4,7 @@
 //const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const baseUrls = {
-  "root": "https://usepicnic.com",
+  "root": "https://dev.usepicnic.com",
   "get-portfolios": "/api/get-portfolios/",
   "get-tvl": "/api/v1/get-tvl/",
 };
